c867e18aaf0e098ee973e2b5aeb05c16b81b5576
Spencer Berenson <spencer@berenson.me>
2018-10-09 17:13:22 -0400

    Derive Webpack entries for frontend by directories inside pages folder

--- webpack.frontend.js
+++ webpack.frontend.js
@@ -1,39 +1,16 @@
 const paths = require("./paths"),
-	{ ProvidePlugin } = require("webpack"),
 	HtmlPlugin = require("html-webpack-plugin");
 
 const config = {
 	entry: {
-		shared: `${paths.frontend.javascript}/shared.js`,
-		home: paths.frontend.home.javascript,
-		information: paths.frontend.information.javascript,
-		academics: paths.frontend.academics.javascript,
-		counseling: paths.frontend.counseling.javascript,
-		extracurriculars: paths.frontend.extracurriculars.javascript,
-		alumni: paths.frontend.alumni.javascript,
-		prospective: paths.frontend.prospective.javascript
+		shared: `${paths.frontend.javascript}/shared.js`
 	},
 	module: {
 		rules: [
 			{
 				test: /.pug$/,
 				include: paths.frontend.partials,
-				use: [
-					{
-						loader: "html-loader",
-						options: {
-							minimize: true,
-							removeAttributeQuotes: false,
-							attrs: [":src"]
-						}
-					},
-					{
-						loader: "pug-html-loader",
-						options: {
-							data: require(paths.frontend.globalData)
-						}
-					}
-				]
+				use: "pug-loader"
 			}
 		]
 	},
@@ -41,6 +18,8 @@ const config = {
 };
 
 for (let page of paths.frontend.pageNames) {
+	config.entry[page] = paths.frontend[page].javascript;
+
 	config.plugins.push(
 		new HtmlPlugin({
 			template: paths.frontend[page].pug,
@@ -54,22 +33,17 @@ for (let page of paths.frontend.pageNames) {
 		include: paths.frontend[page].pug,
 		use: [
 			{
-				loader: "html-loader",
+				loader: "apply-loader",
 				options: {
-					minimize: true,
-					removeAttributeQuotes: false,
-					attrs: [":src"]
+					args: [
+						{
+							...require(paths.frontend.globalData),
+							...require(paths.frontend.localData)[page]
+						}
+					]
 				}
 			},
-			{
-				loader: "pug-html-loader",
-				options: {
-					data: {
-						...require(paths.frontend.globalData),
-						...require(paths.frontend.localData)[page]
-					}
-				}
-			}
+			"pug-loader"
 		]
 	});
 }
