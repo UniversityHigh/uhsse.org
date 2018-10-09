8a5b014860ac22124eef18b309f4bd64fa8ab49d
Spencer Berenson <spencer@berenson.me>
2018-10-08 20:02:14 -0400

    Format files using Prettier

--- src/frontend/pages/home/index.js
+++ src/frontend/pages/home/index.js
@@ -2,10 +2,19 @@ let announcements = $(".announcements p");
 if (announcements.length === 1) {
 	$("nav").append($(".announcements"));
 
-	announcements.on("click", function () {
-		let announcementsLink = announcements.text().match(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig);
-		if(announcementsLink) {
-			if(confirm(announcements.text() + "\n\nClick \"OK\" to proceed to the link.")) {
+	announcements.on("click", function() {
+		let announcementsLink = announcements
+			.text()
+			.match(
+				/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi
+			);
+		if (announcementsLink) {
+			if (
+				confirm(
+					announcements.text() +
+						'\n\nClick "OK" to proceed to the link.'
+				)
+			) {
 				window.open(announcementsLink[0], "_blank");
 			}
 		} else {
@@ -18,14 +27,14 @@ if (announcements.length === 1) {
 	let dataIndex = 0;
 
 	announcements.text(data[dataIndex]);
-	announcements.animateCss("slideInRight", function () {
-		setInterval(function () {
+	announcements.animateCss("slideInRight", function() {
+		setInterval(function() {
 			dataIndex += 1;
 			if (dataIndex >= dataLength) {
 				dataIndex = 0;
 			}
 
-			announcements.animateCss("slideOutLeft", function () {
+			announcements.animateCss("slideOutLeft", function() {
 				announcements.text(data[dataIndex]);
 				announcements.animateCss("slideInRight");
 			});
--- src/frontend/pages/information/index.js
+++ src/frontend/pages/information/index.js
@@ -1,14 +1,27 @@
 $(".aid > a, .update > a").each(function() {
 	let description = $(this).data("description");
-	let descriptionLink = description.match(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig);
+	let descriptionLink = description.match(
+		/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi
+	);
 
-	if(isMobile()) {
-		descriptionLink ? $(this).attr("href", descriptionLink[0]).attr("target", "_blank") : $(this).attr("href", "javascript:alert(\"" + description + "\");");
+	if (isMobile()) {
+		descriptionLink
+			? $(this)
+					.attr("href", descriptionLink[0])
+					.attr("target", "_blank")
+			: $(this).attr("href", 'javascript:alert("' + description + '");');
 	} else {
-		if(descriptionLink) {
-			$(this).attr("href", descriptionLink[0]).attr("target", "_blank");
+		if (descriptionLink) {
+			$(this)
+				.attr("href", descriptionLink[0])
+				.attr("target", "_blank");
 		} else {
-			$(this).attr("href", "javascript:void(0);").attr("title", description).attr("data-toggle", "tooltip").attr("data-trigger", "focus").attr("data-placement", "auto");
+			$(this)
+				.attr("href", "javascript:void(0);")
+				.attr("title", description)
+				.attr("data-toggle", "tooltip")
+				.attr("data-trigger", "focus")
+				.attr("data-placement", "auto");
 			$("[data-toggle='tooltip']").tooltip();
 		}
 	}
