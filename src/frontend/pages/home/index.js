581b874ee3e6f4d41a3c73d74bca61f6d90ca36e
Spencer Berenson <spencer@berenson.me>
2018-10-09 17:18:14 -0400

    Remove Home's old announcement code and import Bulma Carousel

--- src/frontend/pages/home/index.js
+++ src/frontend/pages/home/index.js
@@ -1,49 +1,5 @@
-import "./index.css";
+import "./index.scss";
 
-let announcements = $(".announcements p");
-if (announcements.length === 1) {
-	$("nav").append($(".announcements"));
+import carousel from "bulma-carousel/dist/js/bulma-carousel.min";
 
-	announcements.on("click", function() {
-		let announcementsLink = announcements
-			.text()
-			.match(
-				/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi
-			);
-		if (announcementsLink) {
-			if (
-				confirm(
-					announcements.text() +
-						'\n\nClick "OK" to proceed to the link.'
-				)
-			) {
-				window.open(announcementsLink[0], "_blank");
-			}
-		} else {
-			alert(announcements.text());
-		}
-	});
-
-	let data = announcements.data("announcements");
-	let dataLength = data.length;
-	let dataIndex = 0;
-
-	announcements.text(data[dataIndex]);
-	announcements.animateCss("slideInRight", function() {
-		setInterval(function() {
-			dataIndex += 1;
-			if (dataIndex >= dataLength) {
-				dataIndex = 0;
-			}
-
-			announcements.animateCss("slideOutLeft", function() {
-				announcements.text(data[dataIndex]);
-				announcements.animateCss("slideInRight");
-			});
-		}, 5000);
-	});
-}
-
-$("#gallery").slippry({
-	adaptiveHeight: false
-});
+carousel.attach();
