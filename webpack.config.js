ec1bacef27d65179ce9189529621ca21e1f2ae76
Spencer Berenson <spencer@berenson.me>
2018-10-13 14:59:17 -0400

    Simplify Webpack configuration with ES6 spread operator

--- webpack.config.js
+++ webpack.config.js
@@ -7,96 +7,94 @@ const merge = require("webpack-merge"),
 	WebappPlugin = require("webapp-webpack-plugin");
 
 module.exports = (_env, options) => {
-	const isProduction = options.mode.toLowerCase() === "production",
-		config = merge(require("./webpack.frontend"), {
-			devtool: isProduction ? "source-map" : "inline-source-map",
-			output: {
-				path: path.resolve("dist"),
-				filename: "js/[name]-[hash].js"
-			},
-			module: {
-				rules: [
-					{
-						test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-						loader:
-							"url-loader?limit=10000&mimetype=application/font-woff",
-						options: {
-							name: "[name]-[hash].[ext]",
-							outputPath: "fonts/"
-						}
-					},
-					{
-						test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-						loader: "file-loader",
-						options: {
-							name: "[name]-[hash].[ext]",
-							outputPath: "fonts/"
-						}
-					},
-					{
-						test: /\.(jpg|jpeg|png|ico|svg|gif)$/,
-						use: [
-							{
-								loader: "file-loader",
-								options: {
-									name: "[name]-[hash].[ext]",
-									outputPath: "media/"
-								}
+	const isProduction = options.mode.toLowerCase() === "production";
+
+	return merge(require("./webpack.frontend"), {
+		devtool: isProduction ? "source-map" : "inline-source-map",
+		output: {
+			path: path.resolve("dist"),
+			filename: "js/[name]-[hash].js"
+		},
+		module: {
+			rules: [
+				{
+					test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+					loader:
+						"url-loader?limit=10000&mimetype=application/font-woff",
+					options: {
+						name: "[name]-[hash].[ext]",
+						outputPath: "fonts/"
+					}
+				},
+				{
+					test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+					loader: "file-loader",
+					options: {
+						name: "[name]-[hash].[ext]",
+						outputPath: "fonts/"
+					}
+				},
+				{
+					test: /\.(jpg|jpeg|png|ico|svg|gif)$/,
+					use: [
+						{
+							loader: "file-loader",
+							options: {
+								name: "[name]-[hash].[ext]",
+								outputPath: "media/"
 							}
-						]
-					},
-					{
-						test: /.(sc|c|sa)ss$/,
-						use: [
-							isProduction
-								? MiniCssExtractPlugin.loader
-								: "style-loader",
-							{
-								loader: "css-loader",
-								options: { minimize: "true" }
-							},
-							"postcss-loader",
-							"sass-loader"
-						]
-					},
-					{
-						test: /\.js$/,
-						exclude: /node_modules/,
-						use: ["babel-loader"]
-					}
-				]
-			},
-			plugins: [
-				new ScriptExtHtmlPlugin({
-					defaultAttribute: "defer"
-				}),
-				new MiniCssExtractPlugin({
-					filename: "css/[name]-[hash].css"
-				}),
-				new WebappPlugin({
-					logo: paths.favicon,
-					prefix: "media/",
-					inject: true,
-					favicons: {
-						background: "#fff",
-						theme_color: "#00d1b2",
-						display: "browser",
-						icons: {
-							android: true,
-							appleIcon: true,
-							appleStartup: false,
-							coast: false,
-							favicons: true,
-							firefox: true,
-							windows: true,
-							yandex: true
 						}
-					}
-				})
+					]
+				},
+				{
+					test: /.(sc|c|sa)ss$/,
+					use: [
+						isProduction
+							? MiniCssExtractPlugin.loader
+							: "style-loader",
+						{
+							loader: "css-loader",
+							options: { minimize: "true" }
+						},
+						"postcss-loader",
+						"sass-loader"
+					]
+				},
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: ["babel-loader"]
+				}
 			]
-		});
-
-	if (isProduction) config.plugins.push(new CleanPlugin("dist"));
-
-	return config;
+		},
+		plugins: [
+			new ScriptExtHtmlPlugin({
+				defaultAttribute: "defer"
+			}),
+			new MiniCssExtractPlugin({
+				filename: "css/[name]-[hash].css"
+			}),
+			new WebappPlugin({
+				logo: paths.favicon,
+				prefix: "media/",
+				inject: true,
+				favicons: {
+					background: "#fff",
+					theme_color: "#00d1b2",
+					display: "browser",
+					icons: {
+						android: true,
+						appleIcon: true,
+						appleStartup: false,
+						coast: false,
+						favicons: true,
+						firefox: true,
+						windows: true,
+						yandex: true
+					}
+				}
+			}),
+			...(isProduction ? [new CleanPlugin("dist")] : [])
+		]
+	});
 };
