0dfa776f5df151c6ebb6ff908a594584457f08a6
Spencer Berenson <spencer@berenson.me>
2018-10-13 11:34:34 -0400

    Update paths.js to only call path.resolve once

--- paths.js
+++ paths.js
@@ -9,28 +9,28 @@ const { lstatSync, readdirSync } = require("fs"),
 			.map(name => basename(name)),
 	paths = {
 		frontend: {
-			root: "src/frontend/"
+			root: path.resolve("./src/frontend")
 		}
 	};
 
 function getFrontendPagePaths(page) {
 	return Object.freeze({
-		pug: path.resolve(`${paths.frontend.pages}/${page}/index.pug`),
-		javascript: path.resolve(`${paths.frontend.pages}/${page}/index.js`),
-		css: path.resolve(`${paths.frontend.pages}${page}/index.css`)
+		pug: path.join(paths.frontend.pages, `${page}/index.pug`),
+		javascript: path.join(paths.frontend.pages, `${page}/index.js`),
+		css: path.join(paths.frontend.pages, `${page}/index.css`)
 	});
 }
 
-paths.frontend.pages = path.resolve(`${paths.frontend.root}pages/`);
+paths.frontend.pages = path.join(paths.frontend.root, "pages");
 paths.frontend.pageNames = [];
-paths.frontend.javascript = path.resolve(`${paths.frontend.root}scripts/`);
-paths.frontend.css = path.resolve(`${paths.frontend.root}styles/`);
-paths.frontend.media = path.resolve(`${paths.frontend.root}media/`);
-paths.frontend.partials = path.resolve(`${paths.frontend.root}partials/`);
-paths.frontend.data = path.resolve(`${paths.frontend.root}data/`);
-paths.frontend.globalData = path.resolve(`${paths.frontend.data}/globals.json`);
-paths.frontend.localData = path.resolve(`${paths.frontend.data}/locals.json`);
-paths.favicon = path.resolve(`${paths.frontend.media}/favicon.png`);
+paths.frontend.javascript = path.join(paths.frontend.root, "scripts");
+paths.frontend.css = path.join(paths.frontend.root, "styles");
+paths.frontend.media = path.join(paths.frontend.root, "media");
+paths.frontend.partials = path.join(paths.frontend.root, "partials");
+paths.frontend.data = path.join(paths.frontend.root, "data");
+paths.frontend.globalData = path.join(paths.frontend.data, "globals.json");
+paths.frontend.localData = path.join(paths.frontend.data, "locals.json");
+paths.favicon = path.join(paths.frontend.media, "favicon.png");
 
 for (let frontendPage of getDirectories(paths.frontend.pages)) {
 	paths.frontend[frontendPage] = getFrontendPagePaths(frontendPage);
