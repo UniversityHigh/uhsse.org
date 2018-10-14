bfa62fa7e5d3d0fc37353f64881c2745ba603f60
Spencer Berenson <spencer@berenson.me>
2018-10-14 14:09:50 -0400

    Trigger lozad loading of an image before Siema scrolls to the next slide

--- src/frontend/pages/home/index.js
+++ src/frontend/pages/home/index.js
@@ -1,10 +1,23 @@
 import "./index.scss";
 
+import lozad from "lozad";
 import Siema from "siema";
 
+const observer = lozad();
+
 const gallery = new Siema({
+	draggable: false,
 	duration: 400,
 	loop: true
 });
 
-setInterval(() => gallery.next(), 5000);
+setInterval(() => {
+	observer.triggerLoad(
+		gallery.innerElements[
+			gallery.currentSlide === gallery.innerElements.length - 1
+				? 0
+				: gallery.currentSlide + 1
+		].querySelector("img")
+	);
+	gallery.next();
+}, 5000);
