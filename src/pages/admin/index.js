9758085a39ad703208422efe5cdb0cdb79464455
Spencer Berenson <spencer@berenson.me>
2018-11-22 22:47:22 -0500

    Add Netlify configuration for the Home page

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,4 +1,4 @@
-window.CMS_MANUAL_INIT = false;
+window.CMS_MANUAL_INIT = true;
 
 import CMS, { init } from "netlify-cms";
 
@@ -13,7 +13,90 @@ init({
 			{
 				label: "Pages",
 				name: "pages",
-				files: []
+				create: false,
+				delete: false,
+				editor: {
+					preview: false
+				},
+				files: [
+					{
+						label: "Home",
+						name: "home",
+						file: "src/pages/home/index.json",
+						fields: [
+							{
+								label: "Name",
+								name: "name",
+								widget: "string",
+								hint: "Title at the top of the page"
+							},
+							{
+								label: "Lead",
+								name: "lead",
+								widget: "string",
+								hint: "Text underneath the title"
+							},
+							{
+								label: "Image",
+								name: "image",
+								widget: "image",
+								hint:
+									"Background banner image at the top of the page"
+							},
+							{
+								label: "Phone Number",
+								name: "phoneNumber",
+								widget: "string"
+							},
+							{
+								label: "Directions Link",
+								name: "directionsLink",
+								widget: "string"
+							},
+							{
+								label: "Gallery",
+								name: "gallery",
+								widget: "list",
+								hint:
+									"Slideshow of photos (with optional captions)",
+								allow_add: true,
+								fields: [
+									{
+										label: "Image",
+										name: "link",
+										widget: "image"
+									},
+									{
+										label: "Caption",
+										name: "caption",
+										widget: "string",
+										required: false
+									}
+								]
+							},
+							{
+								label: "Pillars",
+								name: "pillars",
+								widget: "list",
+								hint:
+									"Three important characteristics of the school",
+								allow_add: false,
+								fields: [
+									{
+										label: "Name",
+										name: "name",
+										widget: "string"
+									},
+									{
+										label: "Description",
+										name: "description",
+										widget: "string"
+									}
+								]
+							}
+						]
+					}
+				]
 			}
 		]
 	}
