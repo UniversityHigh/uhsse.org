e7658c1cb08e9173d07d6bbc879b35bbe2120de9
Spencer Berenson <spencer@berenson.me>
2018-11-23 11:37:04 -0500

    Add Netlify CMS configuration for Information page

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -28,21 +28,17 @@ init({
 							{
 								label: "Name",
 								name: "name",
-								widget: "string",
-								hint: "Title at the top of the page"
+								widget: "string"
 							},
 							{
 								label: "Lead",
 								name: "lead",
-								widget: "string",
-								hint: "Text underneath the title"
+								widget: "string"
 							},
 							{
 								label: "Image",
 								name: "image",
-								widget: "image",
-								hint:
-									"Background banner image at the top of the page"
+								widget: "image"
 							},
 							{
 								label: "Phone Number",
@@ -58,8 +54,6 @@ init({
 								label: "Gallery",
 								name: "gallery",
 								widget: "list",
-								hint:
-									"Slideshow of photos (with optional captions)",
 								allow_add: true,
 								fields: [
 									{
@@ -79,8 +73,6 @@ init({
 								label: "Pillars",
 								name: "pillars",
 								widget: "list",
-								hint:
-									"Three important characteristics of the school",
 								allow_add: false,
 								fields: [
 									{
@@ -92,6 +84,401 @@ init({
 										label: "Description",
 										name: "description",
 										widget: "string"
+									}
+								]
+							}
+						]
+					},
+					{
+						label: "Information",
+						name: "information",
+						file: "src/pages/information/index.json",
+						fields: [
+							{
+								label: "Name",
+								name: "name",
+								widget: "string"
+							},
+							{
+								label: "Lead",
+								name: "lead",
+								widget: "string"
+							},
+							{
+								label: "Image",
+								name: "image",
+								widget: "image"
+							},
+							{
+								label: "Contacts",
+								name: "contacts",
+								widget: "object",
+								fields: [
+									{
+										label: "Name",
+										name: "name",
+										widget: "string"
+									},
+									{
+										label: "Lead",
+										name: "lead",
+										widget: "string"
+									},
+									{
+										label: "Anchor ID",
+										name: "anchorId",
+										widget: "hidden"
+									},
+									{
+										label: "Staff",
+										name: "staff",
+										widget: "list",
+										fields: [
+											{
+												label: "Name",
+												name: "name",
+												widget: "string"
+											},
+											{
+												label: "Position",
+												name: "position",
+												widget: "string"
+											},
+											{
+												label: "Email",
+												name: "email",
+												widget: "string"
+											},
+											{
+												label: "Website",
+												name: "website",
+												widget: "string",
+												required: false
+											}
+										]
+									},
+									{
+										label: "Support Services",
+										name: "supportServices",
+										widget: "list",
+										fields: [
+											{
+												label: "Name",
+												name: "name",
+												widget: "string"
+											},
+											{
+												label: "Position",
+												name: "position",
+												widget: "string"
+											},
+											{
+												label: "Email",
+												name: "email",
+												widget: "string"
+											},
+											{
+												label: "Phone",
+												name: "phone",
+												widget: "string"
+											}
+										]
+									}
+								]
+							},
+							{
+								label: "Parent Organizations",
+								name: "parentOrganizations",
+								widget: "object",
+								fields: [
+									{
+										label: "PTO",
+										name: "pto",
+										widget: "object",
+										fields: [
+											{
+												label: "Officers",
+												name: "officers",
+												widget: "list",
+												fields: [
+													{
+														label: "Name",
+														name: "name",
+														widget: "string"
+													},
+													{
+														label: "Position",
+														name: "position",
+														widget: "string"
+													},
+													{
+														label: "Email",
+														name: "email",
+														widget: "string",
+														required: false
+													}
+												]
+											},
+											{
+												label: "Meeting Dates",
+												name: "meetingDates",
+												widget: "object",
+												fields: [
+													{
+														label: "Description",
+														name: "description",
+														widget: "string"
+													},
+													{
+														label: "Dates",
+														name: "dates",
+														widget: "list",
+														field: {
+															label: "Date",
+															name: "date",
+															widget: "date",
+															format:
+																"MMMM D, YYYY"
+														}
+													}
+												]
+											},
+											{
+												label: "Meeting Minutes",
+												name: "meetingMinutes",
+												widget: "list",
+												fields: [
+													{
+														label: "Date",
+														name: "date",
+														widget: "date",
+														format: "MMMM YYYY"
+													},
+													{
+														label: "Link",
+														name: "link",
+														widget: "string"
+													}
+												]
+											},
+											{
+												label: "Fundraisers",
+												name: "fundraisers",
+												widget: "list",
+												fields: [
+													{
+														label: "Name",
+														name: "name",
+														widget: "string"
+													},
+													{
+														label: "Link",
+														name: "link",
+														widget: "string"
+													}
+												]
+											}
+										]
+									},
+									{
+										label: "After Grad",
+										name: "afterGrad",
+										widget: "object",
+										fields: [
+											{
+												label: "Meeting Dates",
+												name: "meetingDates",
+												widget: "object",
+												fields: [
+													{
+														label: "Description",
+														name: "description",
+														widget: "string"
+													},
+													{
+														label: "Dates",
+														name: "dates",
+														widget: "list",
+														field: {
+															label: "Date",
+															name: "date",
+															widget: "date",
+															format:
+																"MMMM D, YYYY"
+														}
+													}
+												]
+											},
+											{
+												label: "Donation Forms",
+												name: "donationForms",
+												widget: "list",
+												fields: [
+													{
+														label: "Name",
+														name: "name",
+														widget: "string"
+													},
+													{
+														label: "Link",
+														name: "link",
+														widget: "string"
+													}
+												]
+											},
+											{
+												label: "Aids",
+												name: "aids",
+												widget: "list",
+												fields: [
+													{
+														label: "Name",
+														name: "name",
+														widget: "string"
+													},
+													{
+														label: "Link",
+														name: "link",
+														widget: "string",
+														required: false
+													}
+												]
+											},
+											{
+												label: "Updates",
+												name: "updates",
+												widget: "list",
+												fields: [
+													{
+														label: "Name",
+														name: "name",
+														widget: "string"
+													},
+													{
+														label: "Link",
+														name: "link",
+														widget: "string",
+														required: false
+													}
+												]
+											}
+										]
+									},
+									{
+										label: "SGC",
+										name: "sgc",
+										widget: "object",
+										fields: [
+											{
+												label: "Meeting Dates",
+												name: "meetingDates",
+												widget: "object",
+												fields: [
+													{
+														label: "Description",
+														name: "description",
+														widget: "string"
+													},
+													{
+														label: "Dates",
+														name: "dates",
+														widget: "list",
+														field: {
+															label: "Date",
+															name: "date",
+															widget: "date",
+															format:
+																"MMMM D, YYYY"
+														}
+													}
+												]
+											},
+											{
+												label: "Meeting Minutes",
+												name: "meetingMinutes",
+												widget: "list",
+												fields: [
+													{
+														label: "Date",
+														name: "date",
+														widget: "date",
+														format: "MMMM YYYY"
+													},
+													{
+														label: "Link",
+														name: "link",
+														widget: "string"
+													}
+												]
+											},
+											{
+												label: "Members",
+												name: "members",
+												widget: "list",
+												fields: [
+													{
+														label: "Name",
+														name: "name",
+														widget: "string"
+													},
+													{
+														label: "Position",
+														name: "position",
+														widget: "string"
+													},
+													{
+														label: "Email",
+														name: "email",
+														widget: "string",
+														required: false
+													}
+												]
+											}
+										]
+									}
+								]
+							},
+							{
+								label: "Resources",
+								name: "resources",
+								widget: "object",
+								fields: [
+									{
+										label: "Name",
+										name: "name",
+										widget: "string"
+									},
+									{
+										label: "lead",
+										name: "lead",
+										widget: "string"
+									},
+									{
+										label: "Anchor ID",
+										name: "anchorId",
+										widget: "hidden"
+									},
+									{
+										label: "Lunch Menu Link",
+										name: "lunchMenuLink",
+										widget: "string"
+									},
+									{
+										label: "Resources",
+										name: "resources",
+										widget: "list",
+										fields: [
+											{
+												label: "Name",
+												name: "name",
+												widget: "string"
+											},
+											{
+												label: "Link",
+												name: "link",
+												widget: "string"
+											}
+										]
 									}
 								]
 							}
