5d9934bef305d5a74caa73fe457874ebdf72786b
Spencer Berenson <spencer@berenson.dev>
2022-12-08 11:59:10 -0500

    feat: use globals for Head title

--- src/lib/nextjs.tsx
+++ src/lib/nextjs.tsx
@@ -1,14 +1,24 @@
+import globals from "@/assets/netlify-cms-globals.json";
 import BaseHead from "next/head";
 
 type HeadProps = {
 	readonly page: string;
 	readonly description: string;
+	readonly titleSeparator?: string;
 };
 
-export const Head = ({ page, description }: HeadProps) => (
+export const Head = ({
+	page,
+	description,
+	titleSeparator = " | ",
+}: HeadProps) => (
 	<BaseHead>
 		<meta content={description} name="description" />
 
-		<title>{page}</title>
+		<title>
+			{page}
+			{titleSeparator}
+			{globals.title}
+		</title>
 	</BaseHead>
 );
