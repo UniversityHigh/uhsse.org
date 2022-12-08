7497cb21808c198810a701b0daca11b1e38c676d
Spencer Berenson <spencer@berenson.dev>
2022-12-08 15:20:25 -0500

    feat: add hero to HomePage

--- src/features/pages/home/components/HomePage.tsx
+++ src/features/pages/home/components/HomePage.tsx
@@ -1,6 +1,8 @@
+import { Hero } from "@/components/Hero";
 import { ChakraProvider } from "@/lib/chakra-ui";
 import { Head, NextPageWithLayout } from "@/lib/nextjs";
 import { getAppLayout } from "@/utils/getAppLayout";
+import netlifyCmsContent from "../assets/netlify-cms-content.json";
 
 const name = "Home";
 const description = "Home of all things UHSSE.";
@@ -8,6 +10,12 @@ const description = "Home of all things UHSSE.";
 export const HomePage: NextPageWithLayout = () => (
 	<ChakraProvider>
 		<Head page={name} description={description} />
+
+		<Hero
+			image={netlifyCmsContent.backgroundImage}
+			heading={netlifyCmsContent.heading}
+			subheading={netlifyCmsContent.subheading}
+		/>
 	</ChakraProvider>
 );
 
--- src/features/pages/home/config/index.ts
+++ src/features/pages/home/config/index.ts
@@ -7,9 +7,24 @@ export const HOME_NETLIFY_COLLECTION_CONFIG: CmsCollectionFile = {
 	file: getNetlifyCmsPageContentPath("home"),
 	fields: [
 		{
-			label: "Foo",
-			name: "foo",
+			label: "Background Image",
+			name: "BackgroundImage",
+			widget: "image",
+			allow_multiple: false,
+			hint: "Image displayed at the top of the page, behind the heading and subheading text. Ideally, this should be a high resolution image.",
+		},
+		{
+			label: "Heading",
+			name: "Heading",
 			widget: "string",
+			hint: "Heading displayed at the top of the page, in front of the background image. Normally, this should be the name of the page.",
+		},
+		{
+			label: "Subheading",
+			name: "Subheading",
+			widget: "string",
+			required: false,
+			hint: "Subheading displayed at the top of the page, below the heading. This should be a short, catchy line that describes the page.",
 		},
 	],
 };
