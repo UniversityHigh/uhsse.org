ecdb08de0a9c87d51085dd9a4f355bfc7adef7c3
Spencer Berenson <spencer@berenson.dev>
2022-12-07 23:07:21 -0500

    fix: use correct variable name

--- src/features/pages/home/config/index.ts
+++ src/features/pages/home/config/index.ts
@@ -1,11 +1,11 @@
-import { NETLIFY_CMS_CONTENT_PATH_RELATIVE_TO_PAGE } from "@/config";
+import { NETLIFY_CMS_CONTENT_PATH } from "@/config";
 import type { CmsCollectionFile } from "netlify-cms-core";
 import path from "path";
 
 export const HOME_NETLIFY_COLLECTION_CONFIG: CmsCollectionFile = {
 	label: "Home",
 	name: "home",
-	file: path.resolve(NETLIFY_CMS_CONTENT_PATH_RELATIVE_TO_PAGE),
+	file: path.resolve(NETLIFY_CMS_CONTENT_PATH),
 	fields: [
 		{
 			label: "Foo",
