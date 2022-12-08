4a0d1971deb229652e7c21ab2c2930929f83c038
Spencer Berenson <spencer@berenson.dev>
2022-12-08 09:54:17 -0500

    feat: set theme brand color to red

--- src/lib/chakra-ui.tsx
+++ src/lib/chakra-ui.tsx
@@ -1,10 +1,24 @@
-import { ChakraProvider as BaseChakraProvider } from "@chakra-ui/react";
+import {
+	baseTheme,
+	ChakraProvider as BaseChakraProvider,
+	extendTheme,
+	withDefaultColorScheme,
+} from "@chakra-ui/react";
 import { ReactNode } from "react";
+
+const theme = extendTheme(
+	{
+		colors: {
+			brand: baseTheme.colors.red,
+		},
+	},
+	withDefaultColorScheme({ colorScheme: "brand" })
+);
 
 type ChakraProviderProps = {
 	readonly children: ReactNode;
 };
 
 export const ChakraProvider = ({ children }: ChakraProviderProps) => (
-	<BaseChakraProvider>{children}</BaseChakraProvider>
+	<BaseChakraProvider theme={theme}>{children}</BaseChakraProvider>
 );
