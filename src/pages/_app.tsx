d5075eda10c75ea6ece4f10f512c9892094eb8d7
Spencer Berenson <spencer@berenson.dev>
2022-12-08 09:20:05 -0500

    chore: add Chakra UI as dependency

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }: AppProps) => (
+	<ChakraProvider>
+		<Component {...pageProps} />
+	</ChakraProvider>
+);
+
+export default App;
