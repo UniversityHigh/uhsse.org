2be5963f7486a043653fbd64d6067942d73324a8
Spencer Berenson <spencer@berenson.dev>
2022-12-09 09:37:13 -0500

    chore: enable linting with type information

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,9 +7,14 @@ module.exports = {
 		"eslint:recommended",
 		"next/core-web-vitals",
 		"plugin:@typescript-eslint/recommended",
+		"plugin:@typescript-eslint/recommended-requiring-type-checking",
 		"plugin:prettier/recommended",
 	],
 	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		tsConfigRootDir: __dirname,
+		project: ["./tsconfig.json"],
+	},
 	plugins: ["@typescript-eslint", "prefer-arrow"],
 	rules: {
 		"@typescript-eslint/consistent-type-imports": "error",
