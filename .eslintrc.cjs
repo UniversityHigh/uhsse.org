304b110347a2dbc90c564c77d5bbb9967719d189
Spencer Berenson <spencer@berenson.dev>
2022-12-09 09:32:50 -0500

    chore: add arrow function and type import ESLint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@
 
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
+	root: true,
 	extends: [
 		"eslint:recommended",
 		"next/core-web-vitals",
@@ -9,6 +10,16 @@ module.exports = {
 		"plugin:prettier/recommended",
 	],
 	parser: "@typescript-eslint/parser",
-	plugins: ["@typescript-eslint"],
-	root: true,
+	plugins: ["@typescript-eslint", "prefer-arrow"],
+	rules: {
+		"@typescript-eslint/consistent-type-imports": "error",
+		"prefer-arrow/prefer-arrow-functions": [
+			"error",
+			{
+				disallowPrototype: true,
+				singleReturnOnly: false,
+				classPropertiesAllowed: false,
+			},
+		],
+	},
 };
