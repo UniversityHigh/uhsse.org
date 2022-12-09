e46a7b68d6bb0a7caa64972457aa290a5ebabc3d
Spencer Berenson <spencer@berenson.dev>
2022-12-09 10:07:44 -0500

    feat: center AnnouncementBar children

--- src/components/AnnouncementBar.tsx
+++ src/components/AnnouncementBar.tsx
@@ -1,5 +1,5 @@
 import { Badge, HStack, Text, Tooltip } from "@chakra-ui/react";
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 
 type AnnouncementBarProps = {
 	readonly children: ReactNode;
@@ -7,17 +7,16 @@ type AnnouncementBarProps = {
 
 export const AnnouncementBar = ({ children }: AnnouncementBarProps) => (
 	<HStack
+		justify="center"
+		backgroundColor="brand.600"
 		paddingX="6"
 		paddingY="3"
-		backgroundColor="brand.600"
-		boxShadow="inner"
 		spacing="4"
-		wordBreak="break-word"
 	>
 		<Badge variant="subtle">Announcement</Badge>
 
 		<Tooltip label={children} placement="bottom-start">
-			<Text fontSize="sm" color="white" noOfLines={1}>
+			<Text color="white" fontSize="sm" noOfLines={1}>
 				{children}
 			</Text>
 		</Tooltip>
